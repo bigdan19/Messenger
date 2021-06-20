90ca0c82dc603cdd15f086e1ea73b335f5965fc8
Daniel Bogucki <bogucki.daniel.alexy@gmail.com>
2021-06-20 23:14:18 +0100

    Getting started with Messenger

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Daniel on 20/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
