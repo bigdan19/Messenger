f57201de8b044e4df7dcb87a2acb8f654f869147
Daniel Bogucki <bogucki.daniel.alexy@gmail.com>
2021-07-26 23:02:11 +0100

    LoginViewController has been modified. Text Fields and Button was added plus a logo

---
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by Daniel on 20/06/2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .link
+    }
+}
